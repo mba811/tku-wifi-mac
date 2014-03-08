873d991093c7619da1efd076868edd7784ff7615
Tommy Chen <tommy351@gmail.com>
2014-03-08 17:31:52 +0800

    Initial Commit

---
+++ tkuwifi/tkuwifi.h
@@ -0,0 +1,15 @@
+//
+//  tkuwifi.h
+//  tkuwifi
+//
+//  Created by SkyArrow on 2014/3/8.
+//  Copyright (c) 2014年 SkyArrow. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface tkuwifi : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ tkuwifi/tkuwifi.m
@@ -0,0 +1,17 @@
+//
+//  tkuwifi.m
+//  tkuwifi
+//
+//  Created by SkyArrow on 2014/3/8.
+//  Copyright (c) 2014年 SkyArrow. All rights reserved.
+//
+
+#import "tkuwifi.h"
+
+@implementation tkuwifi
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
